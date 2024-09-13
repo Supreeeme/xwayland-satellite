00e6b8c38754551073b4a44da51c30b57e4ab58b
Gavin Niederman <gavinniederman@gmail.com>
2024-09-13 14:51:27 -0700

    refactor: use stable nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
