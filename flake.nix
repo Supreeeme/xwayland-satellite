5696b24044e6fbee747779457ffd0c10bb45f748
Gavin-Niederman <gavinniederman@gmail.com>
2024-09-12 19:46:22 -0700

    add: flake

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    naersk.url = "github:nix-community/naersk";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, naersk, flake-utils }:
+    let systems = [ "x86_64-linux" "aarch64-linux" ];
+    in flake-utils.lib.eachSystem systems (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        naersk' = pkgs.callPackage naersk {
+          cargo = pkgs.rust-bin.stable.latest.default;
+          rustc = pkgs.rust-bin.stable.latest.default;
+        };
+      in {
+        devShell = (pkgs.mkShell.override { stdenv = pkgs.clangStdenv; }) {
+          buildInputs = with pkgs; [
+            rustPlatform.bindgenHook
+            rust-bin.stable.latest.default
+            pkg-config
+
+            xcb-util-cursor
+            xorg.libxcb
+            xwayland
+          ];
+        };
+
+        packages = rec {
+          xwayland-satelite = naersk'.buildPackage {
+            src = ./.;
+
+            nativeBuildInputs = with pkgs; [
+              rustPlatform.bindgenHook
+              rust-bin.stable.latest.default
+              pkg-config
+
+              xcb-util-cursor
+              xorg.libxcb
+            ];
+            buildInputs = [ pkgs.xwayland ];
+          };
+
+          default = xwayland-satelite;
+        };
+      });
+}
