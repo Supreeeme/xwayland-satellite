0853fbf8d720d467da65c86b7e108a8f40c1bbde
En-En <39373446+En-En-Code@users.noreply.github.com>
2025-08-27 00:17:25 +0000

    ci: comment logic of set-vars.sh

--- .github/workflows/set-vars.sh
+++ .github/workflows/set-vars.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# Find the line specifying the version number for the Rust toolchain, then
+# extract that version number. Failure to do so aborts the CI run.
 msrv=$(
     awk '/^rust-version = "[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+"$/ {
       print substr($NF, 2, length($NF) - 2)
@@ -10,6 +12,10 @@ if [ -z $msrv ]; then
     exit 1
 fi
 
+# Check for one of two conditions to regenerate the Docker image:
+# (These conditions must be met on `main` and not a PR branch, see ci.yml)
+# 1. An annotated tag was found when fetching the tags at depth 1 (tags on HEAD)
+# 2. "$UBUNTU_DOCKERFILE" was changed between HEAD and the prior commit (HEAD~1)
 git fetch --depth=1 origin +refs/tags/*:refs/tags/*
 if ! git diff HEAD~1 --quiet -- "$UBUNTU_DOCKERFILE" ||
     git describe --candidates=0; then
