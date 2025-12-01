0c8dd0fab4a736f8b6d7abc3f58ad4efa550aa2b
Milo <144418944+pipsuko@users.noreply.github.com>
2025-12-01 08:00:07 +0000

    Update nixpkgs from 25.05 to 25.11
    Fixes #289

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
