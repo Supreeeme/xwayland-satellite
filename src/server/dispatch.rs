82eaca2b2665aafc66b0cb802cb0ed75b562925f
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2026-04-21 22:21:09 +0200

    Set buffer scale based on configured scale
    A workaround for blurry cursor.
    Requires setting XCURSOR_SIZE to double its normal value for
    Xwayland programs. Otherwise you'll get 2x smaller cursor.

--- src/server/dispatch.rs
+++ src/server/dispatch.rs
@@ -167,7 +167,7 @@ impl<S: X11Selection> Dispatch<WlSurface, Entity> for InnerServerState<S> {
                 }
             }
             Request::<WlSurface>::SetBufferScale { scale } => {
-                client.set_buffer_scale(scale);
+                client.set_buffer_scale(state.current_scale as i32);
             }
             Request::<WlSurface>::SetInputRegion { region } => {
                 let region = region.as_ref().map(|r| r.data().unwrap());
